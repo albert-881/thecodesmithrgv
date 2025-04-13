5b03b55b9acc54a5193033131e46ea784b3730cc
albert-881 <quinteroalberto88@gmail.com>
2025-04-12 19:03:54 -0500

    added tmeplate and nice animations and how it works section

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.location.hash) {
+      // Clear the hash and scroll to top
+      history.replaceState(null, null, window.location.pathname);
+      window.scrollTo(0, 0);
+  }
+});
