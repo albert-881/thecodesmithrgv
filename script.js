b088bcf1357fb29df4d931ab2a8f4a6d6497cced
albert-881 <quinteroalberto88@gmail.com>
2025-04-12 21:15:36 -0500

    test

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ menuToggle.addEventListener('click', () => {
 })
 
 document.addEventListener("DOMContentLoaded", function () {
+  
+
   if (window.location.hash) {
       // Clear the hash and scroll to top
       history.replaceState(null, null, window.location.pathname);
